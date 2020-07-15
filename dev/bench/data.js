827ccd987d1767a1f5a0a41e27c4e5ff03ee7aae
github-action-benchmark <github@users.noreply.github.com>
2020-07-15 21:40:51 +0000

    add Rust Benchmark (cargo) benchmark result for 9509a847a51a71cd49757b80cf09a5f3e4c9c5e4

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1594849251892,
+  "repoUrl": "https://github.com/patlux/rust-benchmark-test",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "email@patwoz.de",
+            "name": "Patrick Wozniak",
+            "username": "patlux"
+          },
+          "committer": {
+            "email": "email@patwoz.de",
+            "name": "Patrick Wozniak",
+            "username": "patlux"
+          },
+          "distinct": true,
+          "id": "9509a847a51a71cd49757b80cf09a5f3e4c9c5e4",
+          "message": "chore(ci): adjusting path for output benachmark results",
+          "timestamp": "2020-07-15T23:37:21+02:00",
+          "tree_id": "6b85352d2f482d7dd9e7c34466ba34c0113a1615",
+          "url": "https://github.com/patlux/rust-benchmark-test/commit/9509a847a51a71cd49757b80cf09a5f3e4c9c5e4"
+        },
+        "date": 1594849251546,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "loop/one_loop",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "loop/two_loop",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
